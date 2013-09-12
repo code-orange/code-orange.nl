7228c10ae52b6f0727d73b21639b7efaa05d82ce
Tim van Dalen <Tim@timvdalen.nl>
2013-09-12 12:01:38 +0200

    Update cover, add contact form

--- js/main.js
+++ js/main.js
@@ -371,5 +371,29 @@ $(function(){
 	
 	initMap();
 	
+	$("form").submit(function(){
+		var formdata = {
+			naam: $(this).find("[name=naam]").val(),
+			email: $(this).find("[name=email]").val(),
+			vraag: $(this).find("[name=vraag]").val()
+		};
+		
+		$.ajax({
+			type: "POST",
+			url: "/services/contact_email.php",
+			data: formdata,
+			success: function(){
+				$("form").find("input").val("");
+				$("form").find("textarea").val("Je vraag is ingestuurd");
+			}
+		});
+		
+		return false;
+	});
+	
+	$("form img").click(function(){
+		$("form").submit();
+	});
+	
 	setTimeout(slideBackground, 10000);
 });
---
+++ services/contact_email.php
@@ -0,0 +1,15 @@
+<?php
+	error_reporting(E_ALL);
+
+	$naam = $_REQUEST['naam'];
+	$email = $_REQUEST['email'];
+	$vraag = $_REQUEST['vraag'];
+
+	$to = "nubis-info@timvdalen.nl";
+	$subject = "nub.is vraag van $naam";
+	$message = "van: $naam, $email\nbericht: $vraag\n";
+	$headers = "From: $email". "\r\n" .
+		'Reply-To: $email';
+	
+	mail($to, $subject, $message, $headers);
+?>
