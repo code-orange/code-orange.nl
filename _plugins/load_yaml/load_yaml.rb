7a5ebfda3c56e5a7691f66cca43bc28edaf82011
Tim van Dalen <Tim@timvdalen.nl>
2013-09-22 21:22:16 +0200

    Add template base, add generation of team section

---
+++ _plugins/load_yaml/load_yaml.rb
@@ -0,0 +1,13 @@
+module Jekyll
+  class YAMLoader < Generator
+    priority :highest
+	safe true
+
+	def generate(site)
+		content = {}
+		content['team'] = YAML::load(File.read('_source-assets/content/team.yml'))
+		
+		site.config['content'] = content
+	end
+  end
+end
