67d630a87e5f7de31ac61668ce43f4b54327ef77
Tim van Dalen <Tim@timvdalen.nl>
2013-09-22 21:56:50 +0200

    Generate clients

--- _plugins/load_yaml/load_yaml.rb
+++ _plugins/load_yaml/load_yaml.rb
@@ -5,6 +5,8 @@ module Jekyll
 
 	def generate(site)
 		content = {}
+		
+		content['clients'] = YAML::load(File.read('_source-assets/content/clients.yml'))
 		content['team'] = YAML::load(File.read('_source-assets/content/team.yml'))
 		
 		site.config['content'] = content
